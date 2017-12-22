f7957f4d391b22f65d7f8e809891c08059882820
yanyandrew <30437084+yanyandrew@users.noreply.github.com>
2017-12-22 15:41:42 +0800

    Add files via upload

---
+++ ourClassifier.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 03 07:10:58 2017
+
+@author: Andrew
+"""
+
+from sklearn import svm as egor
+from sklearn import imblearn
+from imblearn.under_sampling import RandomUnderSampler 
+from imblearn.over_sampling import RandomOverSampler 
+
+print dir(egor)
+
+
+def imbalancedClassifier(args*):
+    
+    
+    """MAG BUHAT TAG FUNCTION KUNO NGA MO DETERMINE IF KANUS'A TA MAG OVERSAMPLING OG UNDERSAMPLING"""
+   
+    #DI PA NI MAO
+    RandomUnderSampler()
+    RandomOverSampler()
+    
+    
+    svm.SVC()
